674b3c29cd0a6531971321e9ca7c27e3a182d1ee
Miguel Fernández <mikeloguay@gmail.com>
2024-04-27 19:02:44 +0200

    adding tags

--- main.tf
+++ main.tf
@@ -17,4 +17,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westeurope"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.rg.name
 }
